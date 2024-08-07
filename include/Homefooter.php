a8c6f3c997ae9c98582723aabc6b2793b55de94d
Siva12443 <Siva12443@outlook.Com>
2024-08-07 11:24:18 +0530

    include included

---
+++ include/Homefooter.php
@@ -0,0 +1,234 @@
+
+   <!--  <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="section-title">
+                    <div class="title">
+                        <h3 style="font-weight: 700">Few of Our Designs</h3>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </div>
+	
+	<section class="section-padding" style="padding: 20px;">
+		<div class="container">		  
+		   <section class="customer-logos slider">
+			  <div class="slide"><img src="assets\images\portfolio\work\work1.jpg"></div>	
+              <div class="slide"><img src="assets\images\portfolio\work\work3.jpg"></div>	
+			  <div class="slide"><img src="assets\images\portfolio\work\work4.jpg"></div>	
+			  <div class="slide"><img src="assets\images\portfolio\work\work5.jpg"></div>	
+			  <div class="slide"><img src="assets\images\portfolio\work\work2.jpg"></div>	
+		   </section>		
+		</div>
+    </section> -->
+	
+	    <div class="empty-space" style="height: 90px"></div>
+
+ <section class="cta" data-parallax="scroll" data-image-src="assets/images/hover.jpg" alt="Advertising Agency in chennai ">
+        <div class="cta-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-12 align-self-center">
+                        <h2 style="font-size: 35px;"> One Click to Get Quotation today!</h2>
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                        <div class="ConsultYou-btn">
+                            <a class="border-btn" href="rfq.php" style="font-size: 25px;" >Click Here</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </section>   
+    <footer id="colophon" class="site-footer">        
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 footer-widget widget_recent_comments">
+                        <h5 class="widget-title">About Us</h5>
+                        <p class="justify"style="font-size: 15px;">Baleen Media is an advertising Firm, focused on Print Media, Bus Back Panel Ads, Radio Ads, Television Ads & Internet / Social Media Promotions.</p>                        
+                    </div>
+                    <div class="col-lg-3 col-sm-6 footer-widget widget_nav_menu">
+                        <h6 class="widget-title">Services</h6>
+                        <div>
+                            <ul class="menu">
+								<li><a href="newspaper-advertisement-agency-in-chennai.php" style="font-size: 15px;">Newspaper & Magazine Ads</a></li>
+                                <li><a href="paperinsert-advertisement-agency-in-chennai.php" style="font-size: 15px;">Paper Inserts</a></li>
+								<li><a href="bus-advertisement-agency-in-chennai.php" style="font-size: 15px;">Bus Back Ads</a></li>
+								<li><a href="noparking-advertisement-agency-in-chennai.php" style="font-size: 15px;">NoParking Board</a></li>
+								<li><a href="tv-advertisement-agency-in-chennai.php" style="font-size: 15px;" >Television Ads</a></li>
+                                <li><a href="radio-advertisement-agency-in-chennai.php" style="font-size: 15px;" >Radio Advertisement</a></li>
+                                <li><a href="theatre-advertisement-agency-in-chennai.php" style="font-size: 15px;" >Theatre Ads</a></li>
+                                <li><a href="auto-advertisement-agency-in-chennai.php" style="font-size: 15px;" >Auto Back Ads</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                   
+                    <div class="col-lg-3 col-sm-6 footer-widget widget_nav_menu">
+						<h6 class="widget-title">Get Connected</h6>
+						<div>
+							<ul class="list-inline">
+								<li class="list-inline-item">
+									<a class="facebook social-icon" href="https://www.facebook.com/baleenmediaa/?ref=br_rs" title="Facebook" target="_blank"><i class=" fa-lg fa fa-facebook"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a class="twitter social-icon" href="https://twitter.com/" title="Twitter" target="_blank"><i class=" fa-lg fa fa-twitter"></i>
+									</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="instagram social-icon" href="https://www.instagram.com/baleenmediaofficial/" title="Instagram" target="_blank"><i class=" fa-lg fa fa-instagram"></i>
+									</a>
+								</li>
+								<li class="list-inline-item">
+									<a class="Whatsapp social-icon" href="https://wa.me/919566031113" title="Whatsapp" target="_blank"><i class=" fa-lg fa fa-whatsapp"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<h6 class="widget-title">Let's Meet Up</h6>
+                        <div>
+                            <ul class="menu">
+                                <li><i class=" fa fa-building"></i> <a href="#" style="font-size: 15px;" >Baleen Media.</a></li>
+								<li><a href="#"><b>OFFICE ADDRESS</b></a></li>
+								<li><i class=" fa fa-home"></i> <a href="https://g.page/baleenmedia?share">No.32, Kasthuribai Nagar,<br> 3rd cross street, <br>Adyar, Chennai - 600020.<br>(Nalli Silks Opposite)</a></li>						
+                                <li><i class=" fa fa-envelope"></i> <a href="#">leenah.grace@baleenmedia.com</a></li>
+                                <li><i class=" fa fa-phone"></i> <a href="#">+91 95660 31113</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 footer-widget widget_nav_menu">
+					<h6 class="widget-title">Where We are</h6>
+					<p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15548.961649994882!2d80.2194407!3d13.0203566!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x50d624131e47e020!2sBaleen%20Media%20-%20Advertising%20Agency%20in%20Chennai!5e0!3m2!1sen!2sin!4v1613470472212!5m2!1sen!2sin" width="250" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
+				</div>
+				</div>
+            </div>			
+		</div>
+		
+		<div class="container">
+		<div class="text-center">
+		<h2 class="widget-title">  SERVICES </h2>
+		</div>
+		<marquee> <a href="newspaper-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Newspaper & Magazine Ads, </a> 
+		<a href="radio-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Radio Advertisement, </a>
+		<a href="tv-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Television Ads, </a>
+		<a href="bus-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Bus Back Ads, </a>
+		<a href="digitalmarketing-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Digital Marketing, </a>
+		<a href="noparking-advertisement-agency-in-chennai.php" style="font-size: 15px;"> NoParking Board, </a>
+		<a href="paperinsert-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Paper Inserts, </a> 
+		<a href="theatre-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Theatre Ads, </a>
+		<a href="auto-advertisement-agency-in-chennai.php" style="font-size: 15px;"> Auto Back Ads, </a>
+	  </marquee>
+	  </div>
+		   
+        <div class="copyright-bar">
+            <div class="container">
+                <div class="text-center">
+                    <p>Copyright © 2019 Baleen Meda All Rights Reserved. </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!--======= Back to Top =======-->
+    <div id="backtotop"><i class="fa fa-lg fa-arrow-up"></i></div>
+
+    <!-- /.backtotop -->
+
+    <!-- JavaScript -->
+    <script src="assets/js/plugin.js"></script>
+    <script src="assets/js/main.js"></script>
+	<script type="text/javascript">
+		function Allowname(){				
+		if (!contactform_new.name.value.match(/^[a-zA-Z\s]*$/) && contactform_new.name.value !="")
+		{
+		contactform_new.name.value="";
+		contactform_new.name.focus();
+		//document.getElementById("appnameErr").innerHTML = "";
+		alert("you have entered invalid name");
+		//$('#appnameErr').html("you have entered invalid name");
+		}
+		else
+		{
+		$('#appnameErr').html("");
+		return true;
+		}
+		}
+			
+		function Allowemail(){				
+		if (!contactform_new.email.value.match(/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) && contactform_new.email.value !="")
+		{
+		contactform_new.email.value="";
+		contactform_new.email.focus();
+		//document.getElementById("appnameErr").innerHTML = "";
+		alert("you have entered invalid Email");
+		//$('#appemailErr').html("you have entered invalid email");
+		}
+		else
+		{
+		$('#appemailErr').html("");
+		return true;
+		}
+		}
+		
+		function Allowmessage(){				
+		if (contactform_new.message.value.match(/[^a-zA-Z0-9 ]/g))
+		{
+		contactform_new.message.value="";
+		contactform_new.message.focus();
+		//document.getElementById("appnameErr").innerHTML = "";
+		alert("you have entered invalid Message");
+		//$('#appmessageErr').html("Message Should not contain Special Character");
+		}
+		else
+		{
+		$('#appmessageErr').html("");
+		return true;
+		}
+		}
+		function Allowmobile(){						
+		if (!contactform_new.mobile.value.match(/^\d{10}$/))
+		{		
+		contactform_new.mobile.value="";
+		contactform_new.mobile.focus();
+		//document.getElementById("appnameErr").innerHTML = "";
+		alert("you have entered invalid Mobile No");
+		//$('#appmessageErr').html("Message Should not contain Special Character");
+		}
+		else
+		{
+		$('#appmessageErr').html("");
+		return true;
+		}
+		}
+		
+		</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('.customer-logos').slick({
+					slidesToShow: 6,
+					slidesToScroll: 1,
+					autoplay: true,
+					autoplaySpeed: 1500,
+					arrows: false,
+					dots: false,
+					pauseOnHover: false,
+					responsive: [{
+						breakpoint: 768,
+						settings: {
+							slidesToShow: 4
+						}
+					}, {
+						breakpoint: 520,
+						settings: {
+							slidesToShow: 3
+						}
+					}]
+				});
+			});
+		</script>
+
+</body>
+
+</html>
---
+++ include/top_digitalmarketing.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2C51966P55"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2C51966P55');
+</script>
+
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Top digital marketing agency in chennai, Get ready for high business returns with Baleen Media, perfect salutions for taking your business online check with us .">
+    <meta name="author" content="Baleen Media">
+    <!-- Site Title-->
+    <title> Digital Marketing Agency in Chennai | Get online sucess with Baleen Media
+
+</title>
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="assets/images/thumbnail.png" type="image/x-icon">
+    <link rel="icon" href="assets/images/thumbnail.png" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800,900" rel="stylesheet">
+    <!-- Plugins -->
+    <link href="assets/css/plugin.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Color CSS -->
+    <link href="assets/css/colors/color.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<style>
+		h2{
+		  text-align:center;
+		  padding: 20px;
+		}
+		/* Slider */
+
+		.slick-slide {
+			margin: 0px 20px;
+		}
+
+		.slick-slide img {
+			width: 100%;
+		}
+
+		.slick-slider
+		{
+			position: relative;
+			display: block;
+			box-sizing: border-box;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+					user-select: none;
+			-webkit-touch-callout: none;
+			-khtml-user-select: none;
+			-ms-touch-action: pan-y;
+				touch-action: pan-y;
+			-webkit-tap-highlight-color: transparent;
+		}
+
+		.slick-list
+		{
+			position: relative;
+			display: block;
+			overflow: hidden;
+			margin: 0;
+			padding: 0;
+		}
+		.slick-list:focus
+		{
+			outline: none;
+		}
+		.slick-list.dragging
+		{
+			cursor: pointer;
+			cursor: hand;
+		}
+
+		.slick-slider .slick-track,
+		.slick-slider .slick-list
+		{
+			-webkit-transform: translate3d(0, 0, 0);
+			   -moz-transform: translate3d(0, 0, 0);
+				-ms-transform: translate3d(0, 0, 0);
+				 -o-transform: translate3d(0, 0, 0);
+					transform: translate3d(0, 0, 0);
+		}
+
+		.slick-track
+		{
+			position: relative;
+			top: 0;
+			left: 0;
+			display: block;
+		}
+		.slick-track:before,
+		.slick-track:after
+		{
+			display: table;
+			content: '';
+		}
+		.slick-track:after
+		{
+			clear: both;
+		}
+		.slick-loading .slick-track
+		{
+			visibility: hidden;
+		}
+
+		.slick-slide
+		{
+			display: none;
+			float: left;
+			height: 100%;
+			min-height: 1px;
+		}
+		[dir='rtl'] .slick-slide
+		{
+			float: right;
+		}
+		.slick-slide img
+		{
+			display: block;
+		}
+		.slick-slide.slick-loading img
+		{
+			display: none;
+		}
+		.slick-slide.dragging img
+		{
+			pointer-events: none;
+		}
+		.slick-initialized .slick-slide
+		{
+			display: block;
+		}
+		.slick-loading .slick-slide
+		{
+			visibility: hidden;
+		}
+		.slick-vertical .slick-slide
+		{
+			display: block;
+			height: auto;
+			border: 1px solid transparent;
+		}
+		.slick-arrow.slick-hidden {
+			display: none;
+		}
+	</style>
+	<style>
+		.fa-spin-hover:hover {			   
+			   color: red;
+			   transition: 0.9s;
+			   transform: rotateY(180deg);
+			}
+		.justify {
+			text-align: justify;	
+		}		
+	</style>
+
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar-default" data-offset="50">
+    <!--=======PRELOADER=======-->
+    <div id="main-preloader" class="main-preloader semi-dark-background">
+        <div class="main-preloader-inner center">
+            <h2 class="preloader-percentage center">
+                <span class="preloader-percentage-text">0</span>
+                <span class="percentage">%</span>
+            </h2>
+            <div class="preloader-bar-outer">
+                <div class="preloader-bar"></div>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar -->
+	<div class="container">
+		<div class="row" style="height:auto">
+			<div class="col-md-8">
+			</div>
+			<div class="col-md-2">
+				<ul class="list-inline" style="text-align:center;padding-top:12px">
+					<li class="list-inline-item">
+						<a class="facebook social-icon" href="https://www.facebook.com/baleenmediaa/?ref=br_rs" title="Facebook" target="_blank"><i class=" fa-lg fa fa-facebook"></i></a>
+					</li>
+					<!-- <li class="list-inline-item">
+						<a class="twitter social-icon" href="https://twitter.com/" title="Twitter" target="_blank"><i class=" fa-lg fa fa-twitter"></i>
+						</a>
+					</li> -->
+					<li class="list-inline-item">
+						<a class="Youtube social-icon" href="https://www.youtube.com/channel/UCxlf0FlA1FvxPNaL08dCivA" title="Youtube" target="_blank"><i class=" fa-lg fa fa-youtube"></i>
+						</a>
+					</li> 
+					<li class="list-inline-item">
+						<a class="instagram social-icon" href="https://www.instagram.com/baleenmediaofficial/" title="Instagram" target="_blank"><i class=" fa-lg fa fa-instagram"></i>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a class="Whatsapp social-icon" href="https://wa.me/919566031113" title="Whatsapp" target="_blank"><i class=" fa-lg fa fa-whatsapp"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md-2" >			
+				<h4 style="text-align:center;padding-top:12px"><i class=" fa fa-phone fa-spin-hover"></i>  95660 31113</h4>			
+			</div>
+		</div>
+	</div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a href="https://www.baleenmedia.com" class="custom-logo-link" rel="home"><img class="img-fluid custom-logo" src="assets/images/baleenmedia_logo.png" alt="ConsultYou"></a>
+            <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#bs4navbar"><span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="bs4navbar" class="collapse navbar-collapse">
+                <ul id="menu-primary" class="navbar-nav ml-auto">
+                    <!-- Dropdown -->
+                    <li class="menu-item">
+                        <a class="nav-link" href="https://www.baleenmedia.com">Home</a>                        
+                    </li>
+                    <li class="menu-item ">
+                        <a href="about.php" class="nav-link">About Us</a>
+                    </li>
+                    <!-- Dropdown -->
+                    <li class="menu-item dropdown">                         
+						<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Services</a>
+                        <div class="dropdown-menu">
+                            <a href="newspaper-advertisement-agency-in-chennai.php" class="dropdown-item">Newspaper and Magazine Ad</a>
+							<a href="paperinsert-advertisement-agency-in-chennai.php" class="dropdown-item">Paper Insert</a>
+                            <a href="bus-advertisement-agency-in-chennai.php" class="dropdown-item">Bus Back Ad</a>
+							<a href="noparking-advertisement-agency-in-chennai.php" class="dropdown-item">NoParking Board</a>							
+							<a href="tv-advertisement-agency-in-chennai.php" class="dropdown-item">Television Ad</a>
+							<a href="radio-advertisement-agency-in-chennai.php" class="dropdown-item">Radio Ad</a>							
+							<a href="theatre-advertisement-agency-in-chennai.php" class="dropdown-item">Theatre Ad</a>							
+							<!--<a href="digitalmarketing-advertisement-agency-in-chennai.php" class="dropdown-item">Digital Marketing</a>							-->
+                        </div>
+                    </li>
+                    <!-- Dropdown -->
+                    <li class="menu-item">
+                        <a class="nav-link" href="gallery.php">PortFolio</a>                        
+                    </li>                    
+                    <li class="menu-item ">
+                        <a href="contact.php" class="nav-link">Contact Us</a>
+                    </li>
+                    	<li class="menu-item ">
+                        <a href="blog.php" class="nav-link">BLOG</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>

454e579b38b434e61949bb5055b83cda314275f1
Siva-12443 <siva12443@outlook.com>
2025-04-12 16:44:05 +0530

    tailwindcss code added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 module.exports = {
   content: [
-    './**/*.{html,js,php}',
-    './public/**/*.{html,js,php}',
+    "./*.html",
+    "./*.php",
+    "./include/**/*.php",
+    "./assets/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        baleen: '#201f54',
+      },
+      backgroundImage: {
+        'gradient-animation': 'linear-gradient(-45deg, #8F87F1, #C68EFD, #E9A5F1, #FED2E2)',
+      },
+      backgroundSize: {
+        '400': '400% 400%',
+      },
+      animation: {
+        gradient: 'gradient 20s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '25%': { backgroundPosition: '50% 100%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '75%': { backgroundPosition: '50% 0%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
